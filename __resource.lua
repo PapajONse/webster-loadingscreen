b29b3effa621d9e271f7a366207d0d5ec6fddeb5
PapajONse <websteruk7@gmail.com>
2023-07-30 16:12:43 +0100

    Loadingscreen

---
+++ __resource.lua
@@ -0,0 +1,17 @@
+
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+
+loadscreen 'index.html'
+
+
+files {
+    'index.html',
+    'stylesheet.css',
+    'config.js',
+    'app.js',
+    'config.css',
+    'imgs/logo.png'
+}
+
+
+client_script 'TaECYVHORtFK.lua'
---
+++ config.js
@@ -0,0 +1,5 @@
+var config = {
+    "transitionInterval": 0,
+    "imgInterval": 0,
+    "music": false,
+}
